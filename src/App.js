4464be48d35b67d3e74498ab180b6e87bce0c485
Eric Cascketta <caskman@gmail.com>
2018-09-11 22:36:27 -0500

    reorganized styles

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-import classNames from 'classnames'
 import { ToastContainer, toast } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 
